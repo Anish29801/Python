312b8aa26ac415dd69a7b7d0743f9d7fc28cc1c5
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2023-07-27 18:16:43 +0530

    This is Intial Commit

---
+++ Arhimatic.py
@@ -0,0 +1,12 @@
+import math
+
+def arthimatics(num1, num2):
+    print(num1 + num2)
+    print(num1 - num2)
+    print(num1 * num2)
+    print(math.ceil(num1 / num2))
+    print(num1 // num2)
+    print(num1 ** num2)
+    print(num1 % num2)
+
+arthimatics(5,7)
---
+++ Num.py
@@ -0,0 +1,6 @@
+class Num :
+    def __init__(self, value):
+        for i in range(1,value):
+            print(i)
+
+five = Num(5)
